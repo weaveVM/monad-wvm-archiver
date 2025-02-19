7a4c71312ab50e9edc7dea342d556fbae0729425
charmful0x <charmful0x@gmail.com>
2025-02-19 11:58:29 +0100

    feat: add monad config

--- db_schema.sql
+++ db_schema.sql
@@ -1,19 +1,19 @@
-DROP TABLE IF EXISTS WeaveVMArchiver;
-DROP TABLE IF EXISTS WeaveVMArchiverBackfill;
+DROP TABLE IF EXISTS WeaveVMArchiverMonad;
+DROP TABLE IF EXISTS WeaveVMArchiverMonadBackfill;
 
-CREATE TABLE IF NOT EXISTS WeaveVMArchiver (
+CREATE TABLE IF NOT EXISTS WeaveVMArchiverMonad (
     Id INT AUTO_INCREMENT PRIMARY KEY,
     NetworkBlockId INT UNIQUE,
     WeaveVMArchiveTxid VARCHAR(66) UNIQUE
 );
 
-CREATE TABLE IF NOT EXISTS WeaveVMArchiverBackfill (
+CREATE TABLE IF NOT EXISTS WeaveVMArchiverMonadBackfill (
     Id INT AUTO_INCREMENT PRIMARY KEY,
     NetworkBlockId INT UNIQUE,
     WeaveVMArchiveTxid VARCHAR(66) UNIQUE
 );
 
-CREATE INDEX idx_archiver_txid ON WeaveVMArchiver (WeaveVMArchiveTxid);
-CREATE INDEX idx_backfill_txid ON WeaveVMArchiverBackfill (WeaveVMArchiveTxid);
-CREATE INDEX idx_archiver_block_id ON WeaveVMArchiver (NetworkBlockId);
-CREATE INDEX idx_backfill_block_id ON WeaveVMArchiverBackfill (NetworkBlockId);
+CREATE INDEX idx_archiver_txid ON WeaveVMArchiverMonad (WeaveVMArchiveTxid);
+CREATE INDEX idx_backfill_txid ON WeaveVMArchiverMonadBackfill (WeaveVMArchiveTxid);
+CREATE INDEX idx_archiver_block_id ON WeaveVMArchiverMonad (NetworkBlockId);
+CREATE INDEX idx_backfill_block_id ON WeaveVMArchiverMonadBackfill (NetworkBlockId);
